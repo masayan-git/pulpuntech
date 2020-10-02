ed8deeb6fd8e3bf268cbc004d710de5be62c93ad
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-10-02 17:31:17 +0900

    rootes article => articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :masters
-  root to: 'article#index'
+  root to: 'articles#index'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
