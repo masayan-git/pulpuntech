185924158b75cbe803aa2b3e4a60d6fc4bc6ab6a
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-10-02 17:53:07 +0900

    readme記述　各モデルアソシエーション記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  has_many_attached :image
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  belongs_to :master
+
 end
--- app/models/master.rb
+++ app/models/master.rb
@@ -1,6 +1,8 @@
 class Master < ApplicationRecord
+  has_many :article
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 end
