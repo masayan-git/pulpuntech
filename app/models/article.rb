383fbecf12ccf8f1b3b825d08e26fe219d521a8d
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-10-06 12:25:28 +0900

    バリデーション記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ApplicationRecord
   has_many_attached :image
   has_rich_text :content
-  has_many :article_categories
+  has_many :article_categories, dependent: :delete_all
   has_many :categories, through: :article_categories
   belongs_to :master
+
+  validates :title, presence: true 
+  validates :article_category_ids, presence: true
+  validates :content, presence: true 
 
 end
