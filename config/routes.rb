026de704107cc6a78d9059be8eb3f5ffa6301cad
masayan-git <68589347+masayan-git@users.noreply.github.com>
2020-10-06 15:37:20 +0900

    エラーメッセージ日本語化記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   namespace :masters do
-    resources :tops
+    resources :tops, only: [:index]
     resources :articles
   end
   
-  devise_for :masters
+  
   root to: 'articles#index'
   resources :articles
+
+  devise_for :masters, controllers: {
+    sessions:        'masters/sessions',
+    passwords:       'masters/passwords',
+    registrations:   'masters/registations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
