caf18f4776034a44ce36c6ff7b2939c0ac05f548
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-10-09 10:20:34 +0900

    サイド

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,26 @@
 class MembersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(:name, :infomation, :portfolio).merge(master_id: current_user.id, article_id: params[:article_id])
 end
